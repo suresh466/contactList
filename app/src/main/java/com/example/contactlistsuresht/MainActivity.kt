3bfaf17542f34b877cc61b957d4eddebeb9f2f89
Suresh <suresh.thagunna77@gmail.com>
2024-01-28 17:56:48 -0500

    Wireup the recyclerview and add horizontal divider

--- app/src/main/java/com/example/contactlistsuresht/MainActivity.kt
+++ app/src/main/java/com/example/contactlistsuresht/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.contactlistsuresht
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private var adapter: PersonAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val query = FirebaseDatabase.getInstance().reference.child("people")
+        val options = FirebaseRecyclerOptions.Builder<Person>().setQuery(query, Person::class.java).build()
+        adapter = PersonAdapter(options)
+
+        val rView : RecyclerView = findViewById(R.id.rView)
+        rView.layoutManager = LinearLayoutManager(this)
+        rView.adapter = adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter?.startListening()
     }
 }
