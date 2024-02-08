5fd88a5ef7fad53e16a21cb7051a18056ff25c45
Suresh <suresh.thagunna77@gmail.com>
2024-02-08 13:00:42 -0500

    add deps for wk4: includes glide, storage and auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.0"
+    id("com.google.gms.google-services")
+    id("kotlin-kapt")
 }
 
 android {
@@ -35,6 +35,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+apply(plugin = "com.android.application")
+dependencies {
+    kapt ("com.github.bumptech.glide:compiler:4.11.0")
 }
 
 dependencies {
@@ -43,8 +51,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
-    // Import the BoM for the Firebase platform
+// Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:30.5.0"))
     implementation("com.google.firebase:firebase-core")
     implementation("com.google.firebase:firebase-database")
@@ -52,10 +62,14 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
 
-    implementation ("com.firebaseui:firebase-ui-database:8.0.0")
+    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation ("com.firebaseui:firebase-ui-firestore:8.0.2")
+    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")
+    implementation ("com.firebaseui:firebase-ui-storage:8.0.2")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-apply(plugin = "com.google.gms.google-services")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jetbrains.kotlin.gradle.plugin.*
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+//classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.0"
 }
